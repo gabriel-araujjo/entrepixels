9161cef227da9a82c656702b6516df33953d7b59
Gabriel <gabriel140492@gmail.com>
2016-09-13 15:03:21 -0300

    - Add tests

--- src/io/bitbuf.rs
+++ src/io/bitbuf.rs
@@ -7,11 +7,11 @@ use super::super::error::Error;
 
 pub enum BitSeekFrom {
     Start(u64),
-//    End(i64),
-//    Current(i64),
+    End(i64),
+    Current(i64),
 }
 
-pub struct BitBuf<T> where T: Read + Write + Seek + Sized {
+pub struct BitBuf<T> {
     buf: T,
     current_byte: u8,
     remaining_bits: u8,
@@ -152,25 +152,53 @@ impl<T: Read + Write + Seek + Sized> BitBuf<T> {
                 self.remaining_bits = (8 - pos % 8) as u8;
                 Ok(result * 8 + pos % 8)
             },
-//            BitSeekFrom::Current(pos) => {
-//                let pos = pos - self.remaining_bits as i64;
-//                let byte_pos = pos / 8 - if pos < 8 {1} else {0};
-//                let result = try!(self.buf.seek(SeekFrom::Current(byte_pos)));
-//                self.current_byte = try!(self.read_byte());
-//                if pos > 0 {
-//                    self.remaining_bits = (8 - pos % 8) as u8;
-//                    Ok(result * 8 + (pos % 8) as u64)
-//                } else {
-//                    self.remaining_bits = (- pos % 8) as u8;
-//                    Ok((result + 1) * 8 + (pos % 8) as u64)
-//                }
-//            },
-//            BitSeekFrom::End(pos) => {
-//                let result = try!(self.buf.seek(SeekFrom::End(pos / 8 - 1)));
-//                self.current_byte = try!(self.read_byte());
-//                self.remaining_bits = (- pos % 8) as u8;
-//                Ok((result + 1) * 8 + (pos % 8) as u64)
-//            }
+            BitSeekFrom::Current(pos) => {
+                let pos = pos - self.remaining_bits as i64;
+
+                println!("pos = {}", pos);
+                println!("remaining_bits = {}", self.remaining_bits);
+                let byte_pos = pos / 8 - if pos < 0 && pos % 8 != 0 {1} else {0};
+                println!("byte_pos = {}", byte_pos);
+                let result = try!(self.buf.seek(SeekFrom::Current(byte_pos)));
+                println!("result = {}", result);
+
+                self.current_byte = match self.read_byte() {
+                    Ok(cur_byte) => {
+                        cur_byte
+                    },
+                    Err(err) => {
+                        try!(self.buf.seek(SeekFrom::Current(-byte_pos)));
+                        return Err(err)
+                    }
+                };
+                if pos >= 0 || pos % 8 == 0 {
+                    self.remaining_bits = (8 - pos % 8) as u8;
+                    Ok(result * 8 + (pos % 8) as u64)
+                } else {
+                    self.remaining_bits = (- pos % 8) as u8;
+                    Ok(((result as i64 + 1) * 8 + pos % 8) as u64)
+                }
+            },
+            BitSeekFrom::End(pos) => {
+                let byte_pos = pos / 8 - if pos % 8 != 0 {1} else {0};
+                let result = try!(self.buf.seek(SeekFrom::End(byte_pos)));
+                self.current_byte = match self.read_byte() {
+                    Ok(cur_byte) => {
+                        cur_byte
+                    },
+                    Err(err) => {
+                        try!(self.buf.seek(SeekFrom::Current(-byte_pos)));
+                        return Err(err)
+                    }
+                };
+                if pos % 8 == 0 {
+                    self.remaining_bits = (8 - pos % 8) as u8;
+                    Ok(result * 8 + (pos % 8) as u64)
+                } else {
+                    self.remaining_bits = (- pos % 8) as u8;
+                    Ok(((result as i64 + 1) * 8 + pos % 8) as u64)
+                }
+            }
         }
     }
 
@@ -212,5 +240,104 @@ impl<T: Read + Write + Seek + Sized> BitBuf<T> {
             return Err(Error::new("End of file"))
         }
         Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use super::BitBuf;
+    use super::BitSeekFrom;
+
+    #[test]
+    fn new() {
+        let data = Cursor::new(vec![0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10]);
+        BitBuf::from(data);
+    }
+
+    #[test]
+    fn into_inner() {
+        let data = Cursor::new(vec![0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10]);
+        assert_eq!(&BitBuf::from(data).into_inner().into_inner()[..], &[0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10]);
+    }
+
+    #[test]
+    fn read() {
+        let data = Cursor::new(vec![0x8c, 0x80, 0x41, 0x00]);
+        let mut data = BitBuf::from(data);
+        assert_eq!(data.read(5).unwrap(), 0x11);
+        assert_eq!(data.read(4).unwrap(), 0x09);
+        assert_eq!(data.read(15).unwrap(), 0x41);
+
+        assert_eq!(&data.into_inner().into_inner()[..], &[0x8c, 0x80, 0x41, 0x00]);
+    }
+
+    #[test]
+    fn write() {
+        let data = Cursor::new(vec![0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10]);
+        let mut data = BitBuf::from(data);
+        assert_eq!(data.write(0x1234, 16).unwrap(), 16);
+        assert_eq!(data.write(0x1234, 16).unwrap(), 16);
+        assert_eq!(data.write(0x1234, 16).unwrap(), 16);
+        assert!(data.write(0x1234, 20).is_err());
+
+        assert_eq!(&data.into_inner().into_inner()[..], &[0x12, 0x34, 0x12, 0x34, 0x12, 0x34, 0x01, 0x23]);
+
+        let data = Cursor::new(vec![0x00; 4]);
+        let mut data = BitBuf::from(data);
+        assert_eq!(data.write(0x11, 5).unwrap(), 5);
+        assert_eq!(data.write(0x09, 4).unwrap(), 4);
+        assert_eq!(data.write(0x41, 15).unwrap(), 14);
+
+        assert_eq!(&data.into_inner().into_inner()[..], &[0x8c, 0x80, 0x41, 0x00]);
+    }
+
+    #[test]
+    fn flush() {
+        let mut raw_data = [0x00; 4];
+        let data = Cursor::new(&mut raw_data[..]);
+        let mut data = BitBuf::from(data);
+        assert_eq!(data.write(0x11, 5).unwrap(), 5);
+        assert_eq!(data.write(0x09, 4).unwrap(), 4);
+        assert_eq!(data.write(0x41, 14).unwrap(), 14);
+
+        data.flush().unwrap();
+
+        assert_eq!(&data.into_inner().into_inner()[..], &[0x8c, 0x80, 0x82, 0x00]);
+    }
+
+    #[test]
+    fn seek() {
+        let data = Cursor::new(vec![0x8C, 0x80, 0x82, 0x02]);
+        let mut data = BitBuf::from(data);
+
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+        assert!(data.seek(BitSeekFrom::Current(-1)).is_err());
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+        assert!(data.seek(BitSeekFrom::Current(-5)).is_err());
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+        assert!(data.seek(BitSeekFrom::Current(32)).is_err());
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+
+
+        assert_eq!(data.seek(BitSeekFrom::Current(5)).unwrap(), 5);
+        assert_eq!(data.read(8).unwrap(), 0x90);
+
+        assert_eq!(data.seek(BitSeekFrom::Current(5)).unwrap(), 18);
+        assert_eq!(data.read(8).unwrap(), 0x08);
+
+        assert_eq!(data.seek(BitSeekFrom::Current(-2)).unwrap(), 24);
+        assert_eq!(data.read(7).unwrap(), 0x01);
+
+        assert_eq!(data.seek(BitSeekFrom::Current(-10)).unwrap(), 21);
+        assert_eq!(data.read(8).unwrap(), 0x40);
+
+        assert_eq!(data.seek(BitSeekFrom::End(-32)).unwrap(), 0);
+        assert!(data.seek(BitSeekFrom::End(-33)).is_err());
+        assert_eq!(data.seek(BitSeekFrom::Current(0)).unwrap(), 0);
+        assert_eq!(data.seek(BitSeekFrom::End(-1)).unwrap(), 31);
     }
 }
